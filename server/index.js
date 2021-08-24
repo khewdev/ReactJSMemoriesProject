85ee8554b0340070d1daa2cbad1f918788d920be
khewdev <khewdev@gmail.com>
2021-08-25 06:44:31 +0800

    finish initialize index.js for backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import moongoose from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
@@ -9,4 +9,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://admin:Password123@cluster0.eqd4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch(err => console.log(err.message));
