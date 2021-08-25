740b90004069e6061df3f1e99eb1c16c53801368
khewdev <khewdev@gmail.com>
2021-08-25 21:02:08 +0800

    npm install @material-ui/core

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+//displaying <App /> component on div with id 'root'
+ReactDOM.render(<App />, document.getElementById('root'));
