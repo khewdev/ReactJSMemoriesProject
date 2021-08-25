0c78e86c4f227a7a1c4a5c6adb6d9050e0efb259
khewdev <khewdev@gmail.com>
2021-08-26 00:32:08 +0800

    Completed form design

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import FileBase from 'react-file-base64';
 
 import useStyles from './styles';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
+    });
     const classes = useStyles();
 
+    const clear = () => {
+
+    };
+
+    const handleSubmit = () => {
+
+    };
+
+
+
+
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">Creating a Memory</Typography>
+                <TextField
+                    name="creator"
+                    variant="outlined"
+                    label="Creator"
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+                <TextField
+                    name="title"
+                    variant="outlined"
+                    label="Title"
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+                <TextField
+                    name="message"
+                    variant="outlined"
+                    label="Message"
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+                <TextField
+                    name="tags"
+                    variant="outlined"
+                    label="Tags"
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+                <div className={classes.fileInput}>
+                    <FileBase
+                        type="file"
+                        multiple={false}
+                        onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
+                    />
+                </div>
+                <Button
+                    className={classes.buttonSubmit}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    type="submit"
+                    fullWidth
+                >Submit</Button>
+                <Button
+                    className={classes.buttonSubmit}
+                    variant="contained"
+                    color="secondary"
+                    size="small"
+                    type="submit"
+                    fullWidth
+                    onClick={clear}
+                >Clear</Button>
+
+                {/* if we do in this way, if we have other text field, we would just simply overwrite everything in the postData and left only with creator */}
+                {/* we have to do this instead: onChange={(e) => setPostData({ ...postData, creator: e.target.value })} , this will spread the property in postData and only overwrite creator property*/}
+                {/* onChange={(e) => setPostData({ creator: e.target.value })} */}
+            </form>
+        </Paper>
     );
 };
 
